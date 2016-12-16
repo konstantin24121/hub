659827528e7bc1d741602e4f2b1c27eeeeefa847
Konstantin <konstantin24121@gmail.com>
2016-12-16 11:44:10 +0500

    Configure styleguide
    К стайлгиду добавлены какие есть настройки. Порт на котором запускается
    стайлгайд вынесен в файл настройки окружения .env, в случае если там она
    указана не будет, используется значение основного порта плюс еденица (на
    худой конец запустится на 3001).
    
    Для записи демок используются файл demo.md которой должен лежать в одной
    папке с компонентом.
    
    Issue: #7

--- implements/styleguide.config.js
+++ implements/styleguide.config.js
@@ -3,18 +3,39 @@
 const path = require('path');
 const common = require('./webpack.config.js');
 
-module.exports = {
-	title: 'My Great Style Guide',
-	rootDir: './src',
-	components: 'components/**/index.jsx',
+const port = process.env.STYLEGUIDE_PORT || (+process.env.PORT || 3000) + 1;
+const host = (process.env.HOST || 'localhost');
 
-	updateWebpackConfig(webpackConfig) {
-		const dir = path.resolve(__dirname, 'src');
-		for (const loader of common.module.loaders) {
-			loader.include = dir;
-			webpackConfig.module.loaders.push(loader);
-		}
-		webpackConfig.postcss = common.postcss;
-		return webpackConfig;
-	}
+module.exports = {
+  title: 'StyleGuide',
+  serverPort: port,
+  serverHost: host,
+  highlightTheme: 'material',
+  contextDependencies: [
+    path.resolve(__dirname, '../src/components'),
+  ],
+  getExampleFilename: componentpath => path.join(path.dirname(componentpath), 'demo.md'),
+  getComponentPathLine: (componentpath) => {
+    const dir = path.parse(componentpath).dir;
+    const name = /(\w)*$/.exec(dir);
+    return `import {${name[0]}} from 'components';`;
+  },
+  sections: [
+    {
+      name: 'Introduction',
+    },
+    {
+      name: 'UI Components',
+      components: '../src/components/[A-Z]*/index.jsx',
+    },
+  ],
+  updateWebpackConfig: (webpackConfig) => {
+    const dir = path.resolve(__dirname, '../src');
+    for (const loader of common.module.loaders) {
+      loader.include = dir;
+      webpackConfig.module.loaders.push(loader);
+    }
+    webpackConfig.postcss = common.postcss;
+    return webpackConfig;
+  }
 };
