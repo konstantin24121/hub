5781aafb0eeeaa7a7e7746766c2ee3e662d85447
Meranuxapb <xmeranuxapbx@gmail.com>
2017-04-03 09:48:59 +0500

    Add some changes in markdown and styles for button ripple effect
    кнопка худо-бедно зарябила;
    в стилях пробелы заменены на табы;
    onClick обработчик временно демонтирован в силу
    его прпятствования работе onMouseDown \ onMouseUp,
    позже будет возвращен на место.

--- src/components/RaisedButton/RaisedButton.jsx
+++ src/components/RaisedButton/RaisedButton.jsx
@@ -25,7 +25,12 @@ class RaisedButton extends PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isRipple: false
+			isRipple: false,
+			isClicked: true,
+			isAnimate: false,
+			d: 0,
+			l: 0,
+			t: 0
 		};
 	}
 
@@ -44,19 +49,91 @@ class RaisedButton extends PureComponent {
 	}
 
 	handleMouseDown = (e) => {
+		const mouseEvent = e.nativeEvent;
+		const elementWidth = mouseEvent.target.clientWidth;
+		const elementHeight = mouseEvent.target.clientHeight;
+		let _t = mouseEvent.offsetY;
+		let _l = mouseEvent.offsetX;
+		const box = {
+			tl: {
+				x: 0,
+				y: 0
+			},
+			tr: {
+				x: elementWidth,
+				y: 0
+			},
+			br: {
+				x: elementWidth,
+				y: elementHeight
+			},
+			bl: {
+				x: 0,
+				y: elementHeight
+			},
+			c: {
+				x: elementWidth / 2,
+				y: elementHeight / 2
+			}
+		};
+
+		let corner = {};
+
+		if (_l >= box.c.x) {
+			if (_t >= box.c.y) {
+				corner = box.tl;
+			} else {
+				corner = box.bl;
+			}
+		} else {
+			if (_t >= box.c.y) {
+				corner = box.tr;
+			} else {
+				corner = box.br;
+			}
+		}
+
+		const _r = Math.round( Math.sqrt( Math.pow(corner.x - _l, 2) + Math.pow(corner.y - _t, 2) ) );
+
+		const _d = _r * 2;
+		_l = _l - _r;
+		_t = _t - _r;
+
 		this.setState({
-			isRipple: true
+			isRipple: true,
+			isClicked: false,
+			isAnimate: true,
+			t: _t,
+			l: _l,
+			d: _d
 		});
 	}
 
 	handleMouseUp = (e) => {
-		this.setState({
-			isRipple: false
-		});
+		if (this.state.isAnimate) {
+			this.setState({
+				isClicked: true
+			});
+		} else {
+			this.setState({
+				isClicked: true,
+				isRipple: false
+			})
+		}
 	}
 
 	handleRippleTransitionEnd = (e) => {
-		
+		console.log(e.nativeEvent.propertyName);
+		if (this.state.isClicked) {
+			this.setState({
+				isAnimate: false,
+				isRipple: false
+			});
+		} else {
+			this.setState({
+				isAnimate: false
+			});
+		}
 	}
 
 	/**
@@ -64,11 +141,20 @@ class RaisedButton extends PureComponent {
 	 */
 	render() {
 		const { label } = this.props;
+		const {isRipple, t, l, d} = this.state;
 		const rootCn = cn(s.root);
-		const buttonCn = cn(s.button);
+		const buttonCn = cn(s.button, {
+			[s.isRipple] : isRipple
+		});
 		const rippleAreaCn = cn(s.ripple_area);
 		const rippleWaveCn = cn(s.ripple_wave);
 		const labelCn = cn(s.label);
+		const rippleStyle = {
+			top: t + 'px',
+			left: l + 'px',
+			width: d + 'px',
+			height: d + 'px'
+		};
 
 		return (
 			<div
@@ -76,13 +162,19 @@ class RaisedButton extends PureComponent {
 			>
 				<button
 					className={buttonCn}
-					onClick={this.handleClick}
+					onMouseDown={this.handleMouseDown}
+					onMouseUp={this.handleMouseUp}
 				>
 					<div>
 						<div className={rippleAreaCn}>
-							<div className={rippleWaveCn} onTransitionEnd={this.handleRippleTransitionEnd}></div>
+							<div
+								className={rippleWaveCn}
+								onTransitionEnd={this.handleRippleTransitionEnd}
+								style={rippleStyle}
+							>
+							</div>
 						</div>
-						<div onMouseDown={this.handleMouseDown} onMouseUp={this.handleMouseUp}>
+						<div>
 							<span className={labelCn}>{label}</span>
 						</div>
 					</div>
