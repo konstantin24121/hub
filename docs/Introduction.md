fca7bf6d9af24d3e85170b549e73a95521dd1c7a
Konstantin <konstantin24121@gmail.com>
2017-04-04 14:44:05 +0500

    Right implements and add badges
    Бейджики с количеством открытых issue и версией прилагается

--- docs/Introduction.md
+++ docs/Introduction.md
@@ -1,1 +1,4 @@
+[![GitHub issues](https://img.shields.io/github/issues/konstantin24121/hub.svg?style=flat-square)](https://github.com/konstantin24121/hub/issues)
+[![GitHub release](https://img.shields.io/github/release/konstantin24121/hub.svg?style=flat-square)](https://github.com/konstantin24121/hub)
+
 Перед Вашими глазами Styleguide, по совместительству wiki, для проекта NONAME. Тут можно найти ответы на некоторые вопросы по утройству проекта, по его [запуску и настройке](/#Installation), по приемлемому стилю для написания кода, соглашениям именования, [библиотеке компонентов](/#UI Components) и [примеры](/#Examples) того как компоненты желательно проектировать, а так же краткий экскурс того, как сделать разработку проекта приятной, и использовать все заложенные инструменты.
--- docs/sections.js
+++ docs/sections.js
@@ -1,31 +1,32 @@
 const path = require('path');
+
 const context = process.cwd();
 
 module.exports =
-  [
-    {
-      name: 'Introduction',
-      content: './introduction.md',
-    },
-    {
-      name: 'Documentation',
-      sections: [
-        {
-          name: 'Installation and development',
-          content: './installation.md',
-        },
-        {
-          name: 'Implements and tools',
-          content:'./implements.md',
-        },
-        {
-          name: 'Examples',
-          components: path.resolve(context, 'src/components/__examples__/[A-Z]*/[A-Z]*.jsx'),
-        },
-      ]
-    },
-    {
-      name: 'UI Components',
-      components: path.resolve(context, 'src/components/[A-Z]*/[A-Z]*.jsx'),
-    },
-  ];
+[
+  {
+    name: 'Introduction',
+    content: path.resolve(__dirname, './introduction.md'),
+  },
+  {
+    name: 'Documentation',
+    sections: [
+      {
+        name: 'Installation and development',
+        content: path.resolve(__dirname, './installation.md'),
+      },
+      {
+        name: 'Implements and tools',
+        content: path.resolve(__dirname, './implements.md'),
+      },
+      {
+        name: 'Examples',
+        components: path.resolve(context, 'src/components/__examples__/[A-Z]*/[A-Z]*.jsx'),
+      },
+    ],
+  },
+  {
+    name: 'UI Components',
+    components: path.resolve(context, 'src/components/[A-Z]*/[A-Z]*.jsx'),
+  },
+];
