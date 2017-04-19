2150a7188030237b23b5f502d89a7a8fbeb96f51
Konstantin <konstantin24121@gmail.com>
2017-04-19 21:40:57 +0500

    Add server and styleguide to ecosystem
    Теперь оба сервера можно запустить из под pm2 и наслаждатся
    
    Card: TRL-21

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,21 @@ module.exports = {
     // Static application server
     {
       name: 'server',
-      script: 'npm',
-      args: 'start',
+      script: './implements/server.dev.njs',
+      "env": {
+        "NODE_ENV": "development",
+        "PORT": 3000,
+      }
+    },
+    // Styleguide
+    {
+      name: 'styleguide',
+      script: './node_modules/react-styleguidist/bin/styleguidist.js',
+      args: 'server --config implements/styleguide.config.js',
+      "env": {
+        "NODE_ENV": "development",
+        "STYLEGUIDE_PORT": 3001,
+      }
     }
   ],
 };
